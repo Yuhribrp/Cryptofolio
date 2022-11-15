884419547357a8958130d07e61c8e3f072303fde
YuhriParada <yuhriparada@gmail.com>
2022-11-14 23:05:23 -0300

    cryptos controller

---
+++ app/controllers/cryptos_controller.rb
@@ -0,0 +1,62 @@
+class CryptosController < ApplicationController
+  before_action :set_crypto, only: %i[show edit update destroy]
+
+  def index
+    @cryptos = Crypto.all
+  end
+
+  def show
+  end
+
+  def new
+    @crypto = Crypto.new
+  end
+
+  def edit
+  end
+
+  def create
+    @crypto = Crypto.new(crypto_params)
+
+    respond_to do |format|
+      if @crypto.save
+        format.html { redirect_to crypto_url(@crypto), notice: "Crypto was successfully created." }
+        format.json { render :show, status: :created, location: @crypto }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @crypto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @crypto.update(crypto_params)
+        format.html { redirect_to crypto_url(@crypto), notice: "Crypto was successfully updated." }
+        format.json { render :show, status: :ok, location: @crypto }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @crypto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @crypto.destroy
+
+    respond_to do |format|
+      format.html { redirect_to cryptos_url, notice: "Crypto was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_crypto
+    @crypto = Crypto.find(params[:id])
+  end
+
+  def crypto_params
+    params.require(:crypto).permit(:symbol, :unit_price, :amount_owned)
+  end
+end
